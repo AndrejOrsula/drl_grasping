cf55816c36158d34ddf44f166506d80ae2046dd6
Andrej Orsula <orsula.andrej@gmail.com>
2021-03-02 21:34:00 +0100

    Add Reach tasks with various observations
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

---
+++ drl_grasping/envs/tasks/reach/reach_color_image.py
@@ -0,0 +1,67 @@
+from drl_grasping.envs.tasks.reach import Reach
+from drl_grasping.perception import CameraSubscriber
+from gym_ignition.utils.typing import Observation
+from gym_ignition.utils.typing import ObservationSpace
+from typing import Tuple
+import abc
+import gym
+import numpy as np
+
+
+class ReachColorImage(Reach, abc.ABC):
+
+    # Overwrite parameters for ManipulationGazeboEnvRandomizer
+    _camera_enable: bool = True
+    _camera_type: str = 'camera'
+    _camera_position: Tuple[float, float, float] = (1.1, -0.75, 0.45)
+    _camera_quat_xyzw: Tuple[float, float,
+                             float, float] = (-0.0402991, -0.0166924, 0.9230002, 0.3823192)
+    _camera_ros2_bridge_color: bool = True
+
+    def __init__(self,
+                 agent_rate: float,
+                 restrict_position_goal_to_workspace: bool = True,
+                 shaped_reward: bool = True,
+                 required_accuracy: float = 0.05,
+                 verbose: bool = False,
+                 **kwargs):
+
+        # Initialize the Task base class
+        Reach.__init__(self,
+                       agent_rate=agent_rate,
+                       restrict_position_goal_to_workspace=restrict_position_goal_to_workspace,
+                       shaped_reward=shaped_reward,
+                       required_accuracy=required_accuracy,
+                       verbose=verbose,
+                       **kwargs)
+
+        # Perception (RGB camera)
+        self.camera_sub = CameraSubscriber(topic=f'/{self._camera_type}',
+                                           is_point_cloud=False,
+                                           node_name=f'drl_grasping_rgb_camera_sub_{self.id}')
+
+    def create_observation_space(self) -> ObservationSpace:
+
+        # 0:3*height*width - rgb image
+        return gym.spaces.Box(low=0,
+                              high=255,
+                              shape=(self._camera_height,
+                                     self._camera_width, 3),
+                              dtype=np.uint8)
+
+    def get_observation(self) -> Observation:
+
+        # Get the latest image
+        image = self.camera_sub.get_observation()
+
+        # Reshape and create the observation
+        color_image = np.array(image.data, dtype=np.uint8).reshape(self._camera_height,
+                                                                   self._camera_width, 3)
+
+        observation = Observation(color_image)
+
+        if self._verbose:
+            print(f"\nobservation: {observation}")
+
+        # Return the observation
+        return observation
