5f51e132c5c3f1b13f6a11bb809337a915c4c25a
Andrej Orsula <orsula.andrej@gmail.com>
2021-03-02 21:26:37 +0100

    Remove training world SDF and simplify launch
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- launch/ign_moveit2.launch.py
+++ launch/ign_moveit2.launch.py
@@ -13,33 +13,31 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 def generate_launch_description():
     # Launch Arguments
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
-    config_rviz2 = LaunchConfiguration('config_rviz2', default=os.path.join(get_package_share_directory("drl_grasping"),
-                                                                            "launch", "rviz.rviz"))
+    config_rviz2 = LaunchConfiguration('config_rviz2', default=os.path.join(get_package_share_directory('drl_grasping'),
+                                                                            'launch', 'rviz.rviz'))
+    log_level = LaunchConfiguration('log_level', default='fatal')
 
     return LaunchDescription([
         # Launch Arguments
         DeclareLaunchArgument(
             'use_sim_time',
             default_value=use_sim_time,
-            description='If true, use simulated clock'),
+            description="If true, use simulated clock"),
         DeclareLaunchArgument(
             'config_rviz2',
             default_value=config_rviz2,
-            description='Path to config for RViz2'),
+            description="Path to config for RViz2"),
+        DeclareLaunchArgument(
+            'log_level',
+            default_value=log_level,
+            description="Log level of all nodes launched by this script"),
 
         # MoveIt2 move_group action server with necessary ROS2 <-> Ignition bridges
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                [os.path.join(get_package_share_directory("ign_moveit2"),
-                              "launch", "ign_moveit2.launch.py")]),
+                [os.path.join(get_package_share_directory('ign_moveit2'),
+                              'launch', 'ign_moveit2.launch.py')]),
             launch_arguments=[('use_sim_time', use_sim_time),
-                              ('config_rviz2', config_rviz2)]),
-
-        # Launch world
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                [os.path.join(get_package_share_directory("drl_grasping"),
-                              "launch", "worlds", "world_training_grounds.launch.py")]),
-            launch_arguments=[('use_sim_time', use_sim_time)]),
-
+                              ('config_rviz2', config_rviz2),
+                              ('log_level', log_level)]),
     ])
