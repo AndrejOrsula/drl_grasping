4f80d73910d9d9796a5ba6f0594cb39544eb94c9
Andrej Orsula <orsula.andrej@gmail.com>
2021-03-11 14:50:04 +0100

    Add run script for Docker - Adapted from https://hub.docker.com/r/ignitionrobotics/ignition
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

---
+++ docker/run.bash
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 1 ]
+then
+    echo "Usage: $0 <docker image>"
+    exit 1
+fi
+
+IMG=$1
+ARGS=("$@")
+
+# Make sure processes in the container can connect to the x server
+# Necessary so gazebo can create a context for OpenGL rendering (even headless)
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist $DISPLAY)
+    xauth_list=$(sed -e 's/^..../ffff/' <<< "$xauth_list")
+    if [ ! -z "$xauth_list" ]
+    then
+        echo "$xauth_list" | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+DOCKER_OPTS=
+
+# Get the current version of docker-ce
+# Strip leading stuff before the version number so it can be compared
+DOCKER_VER=$(dpkg-query -f='${Version}' --show docker-ce | sed 's/[0-9]://')
+if dpkg --compare-versions 19.03 gt "$DOCKER_VER"
+then
+    echo "Docker version is less than 19.03, using nvidia-docker2 runtime"
+    if ! dpkg --list | grep nvidia-docker2
+    then
+        echo "Please either update docker-ce to a version greater than 19.03 or install nvidia-docker2"
+  exit 1
+    fi
+    DOCKER_OPTS="$DOCKER_OPTS --runtime=nvidia"
+else
+    DOCKER_OPTS="$DOCKER_OPTS --gpus all"
+fi
+
+# Prevent executing "docker run" when xauth failed.
+if [ ! -f $XAUTH ]
+then
+  echo "[$XAUTH] was not properly created. Exiting..."
+  exit 1
+fi
+
+docker run -it \
+  -e DISPLAY=$DISPLAY \
+  -e QT_X11_NO_MITSHM=1 \
+  -e XAUTHORITY=$XAUTH \
+  -v "$XAUTH:$XAUTH" \
+  -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  -v "/etc/localtime:/etc/localtime:ro" \
+  -v "/dev/input:/dev/input" \
+  -v "$HOME/.ignition:/root/.ignition" \
+  --network host \
+  --ipc host \
+  --rm \
+  -it \
+  --privileged \
+  --security-opt seccomp=unconfined \
+  $DOCKER_OPTS \
+  $IMG \
+  ${@:2}
+
+#   -v "$HOME/ws/training:/root/drl_grasping/training" \
+#   -v "$HOME/ws/pbr_textures:/root/drl_grasping/pbr_textures" \
