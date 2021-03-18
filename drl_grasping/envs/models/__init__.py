527fc2b3cb36d4cfeb5681168c80b6592df8f97b
Andrej Orsula <orsula.andrej@gmail.com>
2021-03-18 18:02:52 +0100

    Add additional models for primitives and random primitive
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- drl_grasping/envs/models/__init__.py
+++ drl_grasping/envs/models/__init__.py
@@ -1,6 +1,6 @@
-from .box import Box
 from .camera import Camera
 from .ground import Ground
 from .panda import Panda
+from .primitives import Box, Cylinder, Sphere, RandomPrimitive
 from .random_ground import RandomGround
 from .random_object import RandomObject
---
+++ drl_grasping/envs/models/primitives/__init__.py
@@ -0,0 +1,4 @@
+from .box import Box
+from .cylinder import Cylinder
+from .sphere import Sphere
+from .random_primitive import RandomPrimitive
