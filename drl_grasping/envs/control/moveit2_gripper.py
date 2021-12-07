254c99fc06e5084b6e90ab34a51c461a8d4d7d4e
Andrej Orsula <orsula.andrej@gmail.com>
2021-12-07 18:40:27 +0100

    Add alternative MoveIt2GripperCommand interface
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- drl_grasping/envs/control/moveit2_gripper.py
+++ drl_grasping/envs/control/moveit2_gripper.py
@@ -3,7 +3,7 @@ from .common import (
     init_follow_joint_trajectory_goal,
 )
 from action_msgs.msg import GoalStatus
-from control_msgs.action import FollowJointTrajectory
+from control_msgs.action import FollowJointTrajectory, GripperCommand
 from moveit_msgs.action import MoveGroup
 from moveit_msgs.msg import Constraints, JointConstraint, MoveItErrorCodes
 from moveit_msgs.srv import GetMotionPlan
@@ -18,10 +18,205 @@ from rclpy.qos import (
 )
 from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectory
-from typing import List, Optional
+from typing import List, Optional, Union
 
 
-class MoveIt2Gripper:
+class MoveIt2GripperCommand:
+    def __init__(
+        self,
+        node: Node,
+        open_gripper_joint_positions: Union[float, List[float]],
+        closed_gripper_joint_positions: Union[float, List[float]],
+        max_effort: float = 0.0,
+        ignore_new_calls_while_executing: bool = True,
+        callback_group: Optional[CallbackGroup] = None,
+        **kwargs,
+    ):
+
+        self._node = node
+
+        # Create action client for move action
+        self.__gripper_command_action_client = ActionClient(
+            node=self._node,
+            action_type=GripperCommand,
+            action_name="gripper_action_controller/gripper_cmd",
+            goal_service_qos_profile=QoSProfile(
+                durability=QoSDurabilityPolicy.VOLATILE,
+                reliability=QoSReliabilityPolicy.RELIABLE,
+                history=QoSHistoryPolicy.KEEP_LAST,
+                depth=1,
+            ),
+            result_service_qos_profile=QoSProfile(
+                durability=QoSDurabilityPolicy.VOLATILE,
+                reliability=QoSReliabilityPolicy.RELIABLE,
+                history=QoSHistoryPolicy.KEEP_LAST,
+                depth=5,
+            ),
+            cancel_service_qos_profile=QoSProfile(
+                durability=QoSDurabilityPolicy.VOLATILE,
+                reliability=QoSReliabilityPolicy.RELIABLE,
+                history=QoSHistoryPolicy.KEEP_LAST,
+                depth=5,
+            ),
+            feedback_sub_qos_profile=QoSProfile(
+                durability=QoSDurabilityPolicy.VOLATILE,
+                reliability=QoSReliabilityPolicy.BEST_EFFORT,
+                history=QoSHistoryPolicy.KEEP_LAST,
+                depth=1,
+            ),
+            status_sub_qos_profile=QoSProfile(
+                durability=QoSDurabilityPolicy.VOLATILE,
+                reliability=QoSReliabilityPolicy.BEST_EFFORT,
+                history=QoSHistoryPolicy.KEEP_LAST,
+                depth=1,
+            ),
+            callback_group=callback_group,
+        )
+
+        self.__open_gripper_command_goal = self.__init_gripper_command_goal(
+            position=open_gripper_joint_positions, max_effort=max_effort
+        )
+        self.__close_gripper_command_goal = self.__init_gripper_command_goal(
+            position=closed_gripper_joint_positions, max_effort=max_effort
+        )
+
+        # Flag that determines whether a new goal can be send while the previous one is being executed
+        self.__ignore_new_calls_while_executing = ignore_new_calls_while_executing
+        if self.__ignore_new_calls_while_executing:
+            self.__is_executing = False
+
+        # Initialize additional variables
+        self.__is_open = True
+
+    def __call__(self):
+
+        self.toggle()
+
+    def toggle(self):
+
+        if self.is_open:
+            self.close()
+        else:
+            self.open()
+
+    def open(self):
+
+        # Don't do anything if open already or if interruptions are not desired
+        if self.is_open:
+            return
+        if self.__ignore_new_calls_while_executing:
+            if self.__is_executing:
+                return
+
+        # Execute
+        self.__send_goal_async_gripper_command(self.__open_gripper_command_goal)
+
+    def close(self):
+
+        # Don't do anything if closed already or if interruptions are not desired
+        if self.is_closed:
+            return
+        if self.__ignore_new_calls_while_executing:
+            if self.__is_executing:
+                return
+
+        # Execute
+        self.__send_goal_async_gripper_command(self.__close_gripper_command_goal)
+
+    @property
+    def is_open(self) -> bool:
+
+        return self.__is_open
+
+    @property
+    def is_closed(self) -> bool:
+
+        return not self.is_open
+
+    def reset_open(self):
+        """
+        Reset controller to an open state.
+        """
+
+        # Execute
+        self.__send_goal_async_gripper_command(self.__open_gripper_command_goal)
+
+        self.__is_open = True
+
+    def reset_closed(self):
+        """
+        Reset controller to a closed state.
+        """
+
+        # Execute
+        self.__send_goal_async_gripper_command(self.__close_gripper_command_goal)
+
+        self.__is_open = False
+
+    def __toggle_internal_gripper_state(self):
+
+        self.__is_open = not self.__is_open
+
+    def __send_goal_async_gripper_command(self, goal: GripperCommand.Goal):
+
+        if not self.__gripper_command_action_client.wait_for_server(timeout_sec=1.0):
+            self._node.get_logger().warn(
+                f"Action server {self.__gripper_command_action_client._action_name} is not yet ready. Better luck next time!"
+            )
+            return
+
+        action_result = self.__gripper_command_action_client.send_goal_async(
+            goal=goal,
+            feedback_callback=None,
+        )
+
+        action_result.add_done_callback(self.__response_callback_gripper_command)
+
+    def __response_callback_gripper_command(self, response):
+
+        goal_handle = response.result()
+        if not goal_handle.accepted:
+            self._node.get_logger().warn(
+                f"Action '{self.__gripper_command_action_client._action_name}' was rejected"
+            )
+            return
+
+        if self.__ignore_new_calls_while_executing:
+            self.__is_executing = True
+
+        self.__get_result_future_gripper_command = goal_handle.get_result_async()
+        self.__get_result_future_gripper_command.add_done_callback(
+            self.__result_callback_gripper_command
+        )
+
+    def __result_callback_gripper_command(self, res):
+
+        if res.result().status == GoalStatus.STATUS_SUCCEEDED:
+            self.__toggle_internal_gripper_state()
+        else:
+            self._node.get_logger().error(
+                f"Action '{self.__gripper_command_action_client._action_name}' was unsuccessful: {res.result().status}"
+            )
+
+        if self.__ignore_new_calls_while_executing:
+            self.__is_executing = False
+
+    @classmethod
+    def __init_gripper_command_goal(
+        cls, position: Union[float, List[float]], max_effort: float
+    ) -> GripperCommand.Goal:
+
+        if hasattr(position, "__getitem__"):
+            position = position[0]
+
+        gripper_cmd_goal = GripperCommand.Goal()
+        gripper_cmd_goal.command.position = position
+        gripper_cmd_goal.command.max_effort = max_effort
+
+        return gripper_cmd_goal
+
+
+class MoveIt2GripperTrajectory:
     def __init__(
         self,
         node: Node,
@@ -34,6 +229,7 @@ class MoveIt2Gripper:
         plan_once: bool = True,
         ignore_new_calls_while_executing: bool = True,
         callback_group: Optional[CallbackGroup] = None,
+        **kwargs,
     ):
 
         self._node = node
@@ -182,9 +378,12 @@ class MoveIt2Gripper:
 
     def open(self):
 
-        # Don't do anything if open already
+        # Don't do anything if open already or if interruptions are not desired
         if self.is_open:
             return
+        if self.__ignore_new_calls_while_executing:
+            if self.__is_executing:
+                return
 
         # Plan only once and execute the same trajectory on each subsequent call
         if self.__plan_once:
@@ -212,9 +411,12 @@ class MoveIt2Gripper:
 
     def close(self):
 
-        # Don't do anything if closed already
+        # Don't do anything if closed already or if interruptions are not desired
         if self.is_closed:
             return
+        if self.__ignore_new_calls_while_executing:
+            if self.__is_executing:
+                return
 
         # Plan only once and execute the same trajectory on each subsequent call
         if self.__plan_once:
@@ -338,10 +540,6 @@ class MoveIt2Gripper:
 
     def __send_goal_async_move_action(self):
 
-        if self.__ignore_new_calls_while_executing:
-            if self.__is_executing:
-                return
-
         stamp = self._node.get_clock().now().to_msg()
         self.__move_action_goal.request.workspace_parameters.header.stamp = stamp
 
@@ -390,10 +588,6 @@ class MoveIt2Gripper:
             self.__is_executing = False
 
     def __send_goal_async_follow_joint_trajectory(self, goal: FollowJointTrajectory):
-
-        if self.__ignore_new_calls_while_executing:
-            if self.__is_executing:
-                return
 
         if not self.__follow_joint_trajectory_action_client.wait_for_server(
             timeout_sec=1.0
@@ -552,3 +746,12 @@ class MoveIt2Gripper:
         del self.__closed_joint_state
         del self.__open_goal_constraints
         del self.__closed_goal_constraints
+
+
+class MoveIt2Gripper(MoveIt2GripperTrajectory):
+    def __init__(
+        self,
+        **kwargs,
+    ):
+
+        super().__init__(**kwargs)
