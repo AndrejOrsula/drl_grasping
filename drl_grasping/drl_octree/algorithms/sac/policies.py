0c3ccdf18206bddb63ef885ae4ec4b665b799c36
Andrej Orsula <orsula.andrej@gmail.com>
2022-02-08 22:10:06 +0100

    Extend DepthImageCnnPolicy for TD3 and SAC
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- drl_grasping/drl_octree/algorithms/sac/policies.py
+++ drl_grasping/drl_octree/algorithms/sac/policies.py
@@ -10,11 +10,17 @@ from stable_baselines3.common.utils import is_vectorized_observation
 from stable_baselines3.sac.policies import Actor, SACPolicy
 from torch import nn
 
-from drl_grasping.drl_octree.features_extractor import OctreeCnnFeaturesExtractor
-from drl_grasping.drl_octree.replay_buffer import preprocess_stacked_octree_batch
+from drl_grasping.drl_octree.features_extractor import (
+    ImageCnnFeaturesExtractor,
+    OctreeCnnFeaturesExtractor,
+)
+from drl_grasping.drl_octree.replay_buffer import (
+    preprocess_stacked_depth_image_batch,
+    preprocess_stacked_octree_batch,
+)
 
 
-class ActorOctreeCnn(Actor):
+class ActorWithoutPreprocessing(Actor):
     """
     Actor network (policy) for SAC.
     Overriden to not preprocess observations (unnecessary conversion into float)
@@ -57,7 +63,7 @@ class ActorOctreeCnn(Actor):
         clip_mean: float = 2.0,
         normalize_images: bool = True,
     ):
-        super(ActorOctreeCnn, self).__init__(
+        super(ActorWithoutPreprocessing, self).__init__(
             observation_space=observation_space,
             action_space=action_space,
             net_arch=net_arch,
@@ -88,7 +94,7 @@ class ActorOctreeCnn(Actor):
         # return self.features_extractor(preprocessed_obs)
 
 
-class ContinuousCriticOctreeCnn(ContinuousCritic):
+class ContinuousCriticWithoutPreprocessing(ContinuousCritic):
     """
     Critic network(s) for DDPG/SAC/TD3.
     Overriden to not preprocess observations (unnecessary conversion into float)
@@ -159,7 +165,6 @@ class ContinuousCriticOctreeCnn(ContinuousCritic):
 class OctreeCnnPolicy(SACPolicy):
     """
     Policy class (with both actor and critic) for SAC.
-    Overriden to not preprocess observations (unnecessary conversion into float)
 
     :param observation_space: Observation space
     :param action_space: Action space
@@ -244,7 +249,7 @@ class OctreeCnnPolicy(SACPolicy):
         actor_kwargs = self._update_features_extractor(
             self.actor_kwargs, features_extractor
         )
-        return ActorOctreeCnn(**actor_kwargs).to(self.device)
+        return ActorWithoutPreprocessing(**actor_kwargs).to(self.device)
 
     def make_critic(
         self, features_extractor: Optional[BaseFeaturesExtractor] = None
@@ -252,7 +257,7 @@ class OctreeCnnPolicy(SACPolicy):
         critic_kwargs = self._update_features_extractor(
             self.critic_kwargs, features_extractor
         )
-        return ContinuousCriticOctreeCnn(**critic_kwargs).to(self.device)
+        return ContinuousCriticWithoutPreprocessing(**critic_kwargs).to(self.device)
 
     def predict(
         self,
@@ -313,4 +318,156 @@ class OctreeCnnPolicy(SACPolicy):
         return actions, state
 
 
+class DepthImageCnnPolicy(SACPolicy):
+    """
+    Policy class (with both actor and critic) for SAC.
+
+    :param observation_space: Observation space
+    :param action_space: Action space
+    :param lr_schedule: Learning rate schedule (could be constant)
+    :param net_arch: The specification of the policy and value networks.
+    :param activation_fn: Activation function
+    :param use_sde: Whether to use State Dependent Exploration or not
+    :param log_std_init: Initial value for the log standard deviation
+    :param sde_net_arch: Network architecture for extracting features
+        when using gSDE. If None, the latent features from the policy will be used.
+        Pass an empty list to use the states as features.
+    :param use_expln: Use ``expln()`` function instead of ``exp()`` when using gSDE to ensure
+        a positive standard deviation (cf paper). It allows to keep variance
+        above zero and prevent it from growing too fast. In practice, ``exp()`` is usually enough.
+    :param clip_mean: Clip the mean output when using gSDE to avoid numerical instability.
+    :param features_extractor_class: Features extractor to use (``OctreeCnnFeaturesExtractor``).
+    :param normalize_images: Whether to normalize images or not,
+         dividing by 255.0 (True by default)
+    :param optimizer_class: The optimizer to use,
+        ``th.optim.Adam`` by default
+    :param optimizer_kwargs: Additional keyword arguments,
+        excluding the learning rate, to pass to the optimizer
+    :param n_critics: Number of critic networks to create.
+    :param share_features_extractor: Whether to share or not the features extractor
+        between the actor and the critic (this saves computation time)
+    """
+
+    def __init__(
+        self,
+        observation_space: gym.spaces.Space,
+        action_space: gym.spaces.Space,
+        # lr_schedule: Schedule, # Note: removed because hinting of Shedule results in import error
+        lr_schedule,
+        net_arch: Optional[Union[List[int], Dict[str, List[int]]]] = None,
+        activation_fn: Type[nn.Module] = nn.ReLU,
+        use_sde: bool = False,
+        log_std_init: float = -3,
+        sde_net_arch: Optional[List[int]] = None,
+        use_expln: bool = False,
+        clip_mean: float = 2.0,
+        features_extractor_class: Type[
+            BaseFeaturesExtractor
+        ] = ImageCnnFeaturesExtractor,
+        features_extractor_kwargs: Optional[Dict[str, Any]] = None,
+        normalize_images: bool = True,
+        optimizer_class: Type[th.optim.Optimizer] = th.optim.Adam,
+        optimizer_kwargs: Optional[Dict[str, Any]] = None,
+        n_critics: int = 2,
+        share_features_extractor: bool = True,
+        separate_networks_for_stacks: bool = True,
+    ):
+        features_extractor_kwargs.update(
+            {"separate_networks_for_stacks": separate_networks_for_stacks}
+        )
+        super(OctreeCnnPolicy, self).__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            net_arch,
+            activation_fn,
+            use_sde,
+            log_std_init,
+            sde_net_arch,
+            use_expln,
+            clip_mean,
+            features_extractor_class,
+            features_extractor_kwargs,
+            normalize_images,
+            optimizer_class,
+            optimizer_kwargs,
+            n_critics,
+            share_features_extractor,
+        )
+
+        self._separate_networks_for_stacks = separate_networks_for_stacks
+
+    def make_actor(
+        self, features_extractor: Optional[BaseFeaturesExtractor] = None
+    ) -> Actor:
+        actor_kwargs = self._update_features_extractor(
+            self.actor_kwargs, features_extractor
+        )
+        return ActorWithoutPreprocessing(**actor_kwargs).to(self.device)
+
+    def make_critic(
+        self, features_extractor: Optional[BaseFeaturesExtractor] = None
+    ) -> ContinuousCritic:
+        critic_kwargs = self._update_features_extractor(
+            self.critic_kwargs, features_extractor
+        )
+        return ContinuousCriticWithoutPreprocessing(**critic_kwargs).to(self.device)
+
+    def predict(
+        self,
+        observation: np.ndarray,
+        state: Optional[np.ndarray] = None,
+        mask: Optional[np.ndarray] = None,
+        deterministic: bool = False,
+    ) -> Tuple[np.ndarray, Optional[np.ndarray]]:
+        """
+        Overriden to create proper Octree batch.
+        Get the policy action and state from an observation (and optional state).
+
+        :param observation: the input observation
+        :param state: The last states (can be None, used in recurrent policies)
+        :param mask: The last masks (can be None, used in recurrent policies)
+        :param deterministic: Whether or not to return deterministic actions.
+        :return: the model's action and the next state
+            (used in recurrent policies)
+        """
+        if not isinstance(observation, dict):
+            observation = np.array(observation)
+
+        vectorized_env = is_vectorized_observation(observation, self.observation_space)
+
+        # Make batch out of tensor (consisting of n-stacked octrees)
+        octree_batch = preprocess_stacked_depth_image_batch(
+            observation,
+            self.device,
+            separate_batches=self._separate_networks_for_stacks,
+        )
+
+        with th.no_grad():
+            actions = self._predict(octree_batch, deterministic=deterministic)
+        # Convert to numpy
+        actions = actions.cpu().numpy()
+
+        if isinstance(self.action_space, gym.spaces.Box):
+            if self.squash_output:
+                # Rescale to proper domain when using squashing
+                actions = self.unscale_action(actions)
+            else:
+                # Actions could be on arbitrary scale, so clip the actions to avoid
+                # out of bound error (e.g. if sampling from a Gaussian distribution)
+                actions = np.clip(
+                    actions, self.action_space.low, self.action_space.high
+                )
+
+        if not vectorized_env:
+            if state is not None:
+                raise ValueError(
+                    "Error: The environment must be vectorized when using recurrent policies."
+                )
+            actions = actions[0]
+
+        return actions, state
+
+
 register_policy("OctreeCnnPolicy", OctreeCnnPolicy)
+register_policy("DepthImageCnnPolicy", DepthImageCnnPolicy)
--- drl_grasping/drl_octree/algorithms/td3/policies.py
+++ drl_grasping/drl_octree/algorithms/td3/policies.py
@@ -10,11 +10,17 @@ from stable_baselines3.common.utils import is_vectorized_observation
 from stable_baselines3.td3.policies import Actor, TD3Policy
 from torch import nn
 
-from drl_grasping.drl_octree.features_extractor import OctreeCnnFeaturesExtractor
-from drl_grasping.drl_octree.replay_buffer import preprocess_stacked_octree_batch
+from drl_grasping.drl_octree.features_extractor import (
+    ImageCnnFeaturesExtractor,
+    OctreeCnnFeaturesExtractor,
+)
+from drl_grasping.drl_octree.replay_buffer import (
+    preprocess_stacked_depth_image_batch,
+    preprocess_stacked_octree_batch,
+)
 
 
-class ActorOctreeCnn(Actor):
+class ActorWithoutPreprocessing(Actor):
     """
     Actor network (policy) for TD3.
     Overriden to not preprocess observations (unnecessary conversion into float)
@@ -40,7 +46,7 @@ class ActorOctreeCnn(Actor):
         activation_fn: Type[nn.Module] = nn.ReLU,
         normalize_images: bool = True,
     ):
-        super(ActorOctreeCnn, self).__init__(
+        super(ActorWithoutPreprocessing, self).__init__(
             observation_space=observation_space,
             action_space=action_space,
             net_arch=net_arch,
@@ -65,7 +71,7 @@ class ActorOctreeCnn(Actor):
         # return self.features_extractor(preprocessed_obs)
 
 
-class ContinuousCriticOctreeCnn(ContinuousCritic):
+class ContinuousWithoutPreprocessing(ContinuousCritic):
     """
     Critic network(s) for DDPG/SAC/TD3.
     Overriden to not preprocess observations (unnecessary conversion into float)
@@ -136,7 +142,6 @@ class ContinuousCriticOctreeCnn(ContinuousCritic):
 class OctreeCnnPolicy(TD3Policy):
     """
     Policy class (with both actor and critic) for TD3.
-    This class includes OctreeCnnFeaturesExtractor by default
 
     :param observation_space: Observation space
     :param action_space: Action space
@@ -203,7 +208,7 @@ class OctreeCnnPolicy(TD3Policy):
         actor_kwargs = self._update_features_extractor(
             self.actor_kwargs, features_extractor
         )
-        return ActorOctreeCnn(**actor_kwargs).to(self.device)
+        return ActorWithoutPreprocessing(**actor_kwargs).to(self.device)
 
     def make_critic(
         self, features_extractor: Optional[BaseFeaturesExtractor] = None
@@ -211,7 +216,7 @@ class OctreeCnnPolicy(TD3Policy):
         critic_kwargs = self._update_features_extractor(
             self.critic_kwargs, features_extractor
         )
-        return ContinuousCriticOctreeCnn(**critic_kwargs).to(self.device)
+        return ContinuousWithoutPreprocessing(**critic_kwargs).to(self.device)
 
     def predict(
         self,
@@ -272,4 +277,138 @@ class OctreeCnnPolicy(TD3Policy):
         return actions, state
 
 
+class DepthImageCnnPolicy(TD3Policy):
+    """
+    Policy class (with both actor and critic) for TD3.
+
+    :param observation_space: Observation space
+    :param action_space: Action space
+    :param lr_schedule: Learning rate schedule (could be constant)
+    :param net_arch: The specification of the policy and value networks.
+    :param activation_fn: Activation function
+    :param features_extractor_class: Features extractor to use.
+    :param features_extractor_kwargs: Keyword arguments
+        to pass to the features extractor.
+    :param normalize_images: Whether to normalize images or not,
+         dividing by 255.0 (True by default)
+    :param optimizer_class: The optimizer to use,
+        ``th.optim.Adam`` by default
+    :param optimizer_kwargs: Additional keyword arguments,
+        excluding the learning rate, to pass to the optimizer
+    :param n_critics: Number of critic networks to create.
+    :param share_features_extractor: Whether to share or not the features extractor
+        between the actor and the critic (this saves computation time)
+    """
+
+    def __init__(
+        self,
+        observation_space: gym.spaces.Space,
+        action_space: gym.spaces.Space,
+        lr_schedule,
+        net_arch: Optional[Union[List[int], Dict[str, List[int]]]] = None,
+        activation_fn: Type[nn.Module] = nn.ReLU,
+        features_extractor_class: Type[
+            BaseFeaturesExtractor
+        ] = ImageCnnFeaturesExtractor,
+        features_extractor_kwargs: Optional[Dict[str, Any]] = None,
+        normalize_images: bool = True,
+        optimizer_class: Type[th.optim.Optimizer] = th.optim.Adam,
+        optimizer_kwargs: Optional[Dict[str, Any]] = None,
+        n_critics: int = 2,
+        share_features_extractor: bool = True,
+        separate_networks_for_stacks: bool = True,
+    ):
+        features_extractor_kwargs.update(
+            {"separate_networks_for_stacks": separate_networks_for_stacks}
+        )
+        super(OctreeCnnPolicy, self).__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            net_arch,
+            activation_fn,
+            features_extractor_class,
+            features_extractor_kwargs,
+            normalize_images,
+            optimizer_class,
+            optimizer_kwargs,
+            n_critics,
+            share_features_extractor,
+        )
+
+        self._separate_networks_for_stacks = separate_networks_for_stacks
+
+    def make_actor(
+        self, features_extractor: Optional[BaseFeaturesExtractor] = None
+    ) -> Actor:
+        actor_kwargs = self._update_features_extractor(
+            self.actor_kwargs, features_extractor
+        )
+        return ActorWithoutPreprocessing(**actor_kwargs).to(self.device)
+
+    def make_critic(
+        self, features_extractor: Optional[BaseFeaturesExtractor] = None
+    ) -> ContinuousCritic:
+        critic_kwargs = self._update_features_extractor(
+            self.critic_kwargs, features_extractor
+        )
+        return ContinuousWithoutPreprocessing(**critic_kwargs).to(self.device)
+
+    def predict(
+        self,
+        observation: np.ndarray,
+        state: Optional[np.ndarray] = None,
+        mask: Optional[np.ndarray] = None,
+        deterministic: bool = False,
+    ) -> Tuple[np.ndarray, Optional[np.ndarray]]:
+        """
+        Overriden to create proper Octree batch.
+        Get the policy action and state from an observation (and optional state).
+
+        :param observation: the input observation
+        :param state: The last states (can be None, used in recurrent policies)
+        :param mask: The last masks (can be None, used in recurrent policies)
+        :param deterministic: Whether or not to return deterministic actions.
+        :return: the model's action and the next state
+            (used in recurrent policies)
+        """
+        if not isinstance(observation, dict):
+            observation = np.array(observation)
+
+        vectorized_env = is_vectorized_observation(observation, self.observation_space)
+
+        # Make batch out of tensor (consisting of n-stacked octrees)
+        octree_batch = preprocess_stacked_depth_image_batch(
+            observation,
+            self.device,
+            separate_batches=self._separate_networks_for_stacks,
+        )
+
+        with th.no_grad():
+            actions = self._predict(octree_batch, deterministic=deterministic)
+        # Convert to numpy
+        actions = actions.cpu().numpy()
+
+        if isinstance(self.action_space, gym.spaces.Box):
+            if self.squash_output:
+                # Rescale to proper domain when using squashing
+                actions = self.unscale_action(actions)
+            else:
+                # Actions could be on arbitrary scale, so clip the actions to avoid
+                # out of bound error (e.g. if sampling from a Gaussian distribution)
+                actions = np.clip(
+                    actions, self.action_space.low, self.action_space.high
+                )
+
+        if not vectorized_env:
+            if state is not None:
+                raise ValueError(
+                    "Error: The environment must be vectorized when using recurrent policies."
+                )
+            actions = actions[0]
+
+        return actions, state
+
+
 register_policy("OctreeCnnPolicy", OctreeCnnPolicy)
+register_policy("DepthImageCnnPolicy", DepthImageCnnPolicy)
